50e9ba8c95178c4dc0ea541948c61477e84c0fdd
Meghanath91 <meghanath.balaji@gmail.com>
2020-05-10 04:52:43 -0400

    services setup

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{config('app.name','Labs')}}</title>
-
-        <!-- Fonts -->
-
-    </head>
-    <body>
-<h1> Welcome to Labs</h1>
+@extends('layouts.app')
+@section('content')
+<h1> {{$title}}</h1>
 <p>laravel from scratch </p>
-    </body>
-</html>
+@endsection
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,17 +1,11 @@
+@extends('layouts.app')
+@section('content')
 
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{config('app.name','Labs')}}</title>
-
-        <!-- Fonts -->
-
-    </head>
-    <body>
-<h1> Services</h1>
-<p>this is about services </p>
-    </body>
-</html>
+<h1> {{$title}}</h1>
+@if(count($services)>0)
+    <ul>
+         @foreach($services as $service)
+                <li>{{$service}}</li>
+        @endforeach
+    @endif
+ @endsection
